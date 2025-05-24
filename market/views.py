7be8b87f0ff352db351b9909db40c9fed09a82a6
Andrey <fatym98@gmail.com>
2025-05-24 22:09:06 +0400

    finish_app

--- market/views.py
+++ market/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
-# Create your views here.
+from market.models import Consumer, Product, Supplier
+from market.serializers import (ConsumerSerializer, ProductSerializer,
+                                SupplierSerializer)
+from users.permissions import IsActiveUser
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveUser]
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["country"]
+    permission_classes = [IsActiveUser]
+
+
+class ConsumerViewSet(viewsets.ModelViewSet):
+    queryset = Consumer.objects.all()
+    serializer_class = ConsumerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["country"]
+    permission_classes = [IsActiveUser]
